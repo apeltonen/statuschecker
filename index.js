6b38ae24280c4d36cff4946fe00fb3f5b8816026
Aleksi Peltonen <aleksipeltonen@Aleksis-Mini.home>
2021-07-07 13:06:32 +0300

    add .env

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const fetch = require('node-fetch')
 const nodemailer = require("nodemailer");
 
 
 
-const sendMail = async (viesti) => {
+const sendMail = async (otsikko, viesti) => {
   
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
@@ -11,8 +14,8 @@ const sendMail = async (viesti) => {
       port: 465,
       secure: true, // true for 465, false for other ports
       auth: {
-        user: 'clubservice@fairytalemagic.fi', // generated ethereal user
-        pass: 'CLu34Pp)_2018', // generated ethereal password
+        user: process.env.USER, // generated ethereal user
+        pass: process.env.PASSWORD, // generated ethereal password
       },
     });
   
@@ -20,7 +23,7 @@ const sendMail = async (viesti) => {
     let info = await transporter.sendMail({
       from: '"Club App" <clubservice@fairytalemagic.fi>', // sender address
       to: "fairytalemagicltd@gmail.com", // list of receivers
-      subject: "HÄLYTYS", // Subject line
+      subject: `${otsikko}`, // Subject line
       text: `${viesti}`, // plain text body
       html: `<b>${viesti}</b>`, // html body
     });
@@ -34,6 +37,7 @@ const sendMail = async (viesti) => {
   }
 
 let oldValues = {ncount: 0, pncount: 0}
+let counter = 1
 
 const checker = () => {
     const currentDate = new Date();
@@ -56,20 +60,22 @@ const checker = () => {
             let ago= Math.round((timestamp-lnts)/1000/60)
             let lago= Math.round((timestamp-lpts)/1000/60)
             console.log(`\n\n---------------------------------------------------------------------------------\n\nChecker-ajo käynnistyi ${d}\n\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\nEdellisen ajon aikana/Tämän ajon aikana saatu uutisten määrä tietokannassa: ${oldValues['ncount']}/${nCount}\n\nEdellisen ajon aikana/Tämän ajon aikana saatu lähetettyjen herätteiden määrä tietokannassa: ${oldValues['pncount']}/${pnCount}\n\nViimeksi haettu tai päivitetty uutisia:${dn} eli ${ago}min sitten.\n\nViimeksi lähetetty herätteitä:${dp} eli ${lago}min sitten.\n`)
-            if ((timestamp-lnts) >= delay){ //sendMail(`Herätteissä on häiriö. Viimeisen kerran herätteitä on haettu ${ago}min sitten`).catch(console.error)
+            if (counter == 36) sendMail('Aleksi testailee',`\n\n---------------------------------------------------------------------------------\n\nChecker-ajo käynnistyi ${d}\n\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\nEdellisen ajon aikana/Tämän ajon aikana saatu uutisten määrä tietokannassa: ${oldValues['ncount']}/${nCount}\n\nEdellisen ajon aikana/Tämän ajon aikana saatu lähetettyjen herätteiden määrä tietokannassa: ${oldValues['pncount']}/${pnCount}\n\nViimeksi haettu tai päivitetty uutisia:${dn} eli ${ago}min sitten.\n\nViimeksi lähetetty herätteitä:${dp} eli ${lago}min sitten.\n`).catch(console.error)
+            console.log(`\nCOUNTER: ${counter}\n`)
+            if ((timestamp-lnts) >= delay){
+              //sendMail('VIKATILANNE',`Herätteissä on häiriö. Viimeisen kerran herätteitä on haettu ${ago} min sitten. Virhekoodi A.`).catch(console.error)
               console.log(`A: Herätteissä on häiriö. Viimeisen kerran herätteitä on haettu ${ago}min sitten`)
             }
-            else if ((timestamp-lpts) >= delay){
-              lago= Math.round((timestamp-lpts)/1000/60)
-               //sendMail(`Herätteissä on häiriö. Viimeisen kerran push-herätteitä on lähtenyt ${ago}min sitten`).catch(console.error)
+            else if ((timestamp-lpts) >= delay){  
+               //sendMail('VIKATILANNE',`Herätteissä on häiriö. Viimeisen kerran push-herätteitä on lähtenyt ${lago} min sitten. Virhekoodi B.`).catch(console.error)
                console.log(`B: Herätteissä on häiriö. Viimeisen kerran push-herätteitä on lähtenyt ${lago}min sitten`)
             }
             else if (nCount <= oldValues['ncount'] && oldValues['ncount'] !== 0){
-              //sendMail(`Herätteissä on häiriö tai uutisia on poistettu tietokannasta, sillä niitä on saman verran tai vähemmän kuin 40min sitten.`).catch(console.error)
+              //sendMail('VIKATILANNE',`Herätteissä on häiriö tai uutisia on poistettu tietokannasta, sillä niitä on saman verran tai vähemmän kuin 40 min sitten. Virhekoodi C.`).catch(console.error)
               console.log(`C: Herätteissä on häiriö tai uutisia on poistettu tietokannasta, sillä niitä on saman verran tai vähemmän kuin 40min sitten.`)
             }
             else if (pnCount == oldValues['pncount'] && oldValues['ncount'] !== 0){
-               //sendMail(`Herätteissä on häiriö, sillä push herätteitä ei ole lähtenyt 40minuuttiin, vaikka uutisia on haettu.`).catch(console.error)
+               //sendMail('VIKATILANNE',`Herätteissä on häiriö, sillä push herätteitä ei ole lähtenyt 40 minuuttiin, vaikka uutisia on haettu. Virhekoodi D.`).catch(console.error)
                console.log(`D: Herätteissä on häiriö, sillä push herätteitä ei ole lähtenyt 40minuuttiin, vaikka uutisia on haettu.`)
             }
             //console.log(`\n minuutteja: ${ago}`)
@@ -77,6 +83,8 @@ const checker = () => {
             oldValues['pncount']=pnCount
             //console.log(`\n ${oldValues['ncount']} \n ${oldValues['pncount']}`)
             console.log(`\n\n---------------------------------------------------------------------------------\n\n`)
+            if (counter == 36) counter = 1
+            else counter++
         })
 }
-setInterval(() =>  {checker()},5000) //2400000
+setInterval(() =>  {checker()},2400000)
